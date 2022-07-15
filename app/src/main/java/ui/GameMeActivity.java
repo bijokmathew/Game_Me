cb6e00cec437f00c639a45188b9e4b7f2147ea0e
bijokmathew <bijo.nbr@hotmail.com>
2022-07-15 22:59:23 +0100

    added GameMe resources and convert all resources in to java code

---
+++ app/src/main/java/ui/GameMeActivity.java
@@ -0,0 +1,16 @@
+package ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.bkm.game_me.R;
+
+public class GameMeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
